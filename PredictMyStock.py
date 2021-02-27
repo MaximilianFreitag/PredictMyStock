0171b9a0e60b25626a240005e89c3c9c47ab558d
Maximilian Freitag <46624616+MaximilianFreitag@users.noreply.github.com>
2021-02-27 15:54:34 +0100

    Update PredictMyStock.py

--- PredictMyStock.py
+++ PredictMyStock.py
@@ -7,7 +7,6 @@
 # pip install streamlit fbprophet yfinance plotly
 import streamlit as st
 from datetime import date
-
 import yfinance as yf
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
@@ -40,17 +39,17 @@ def _max_width_():
 
 
 
-START = "2010-01-01"
+START = "2000-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
-st.markdown("<h1 style='text-align: center; color: black;'>Predict My Stock</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: black;'>Predict My Stocks</h1>", unsafe_allow_html=True)
 
-st.markdown("<h3 style='text-align: center; color: black;'>Enter your stock and choose how far you want the machine learning code to forcast the result </h3>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color: black;'>Enter your stock and wait 10-20 seconds for the machine learning code to process the data </h2>", unsafe_allow_html=True)
 
 
 #https://ibb.co/FqhFwD3
 
-st.markdown("<h5 style='text-align: center; color: black;'>(Please note that the results are not a guarantee. With this project I wanted to enable people to apply machine learning to their stocks. If an error occurs the stock is not in my stock list.)</h5>", unsafe_allow_html=True)
+st.markdown("<h5 style='text-align: center; color: black;'>(Please note that the results are not a guarantee. With this project I wanted to enable people to apply machine learning to their stocks. If an error occurs the stock is not in my stock list. Prices are in US dollars. Version 2.0.0 Added cryptocurrencies BTC-USD)</h5>", unsafe_allow_html=True)
 
 st.markdown("<h5 style='text-align: center; color: black;'>                 </h5>", unsafe_allow_html=True)
 
@@ -59,7 +58,7 @@ st.markdown("<h5 style='text-align: center; color: black;'>                 </h5
 
 stocks = (' AAPL', ' GOOG', ' AMZN', ' TSLA', ' FB', ' GME', ' MSFT', ' ADBE', ' ORCL', ' SNPS', ' VRSN', ' ACN', ' IBM', ' CRM', ' NOW', ' FIS',
 ' FISV', ' ADSK', ' INTU', ' COMMU', ' CSCO', ' AMAT', ' APH', ' HPQ', ' MSI', ' V', ' DIS', ' CMCSA', ' VZ', ' T', ' TMUS', ' NFLX', ' CHTR',
-' NVDA', ' AVGO', ' QCOM', ' TXN', ' MU', ' ADI', ' AMD', ' XLNX', ' MCHP', ' ADI', ' JPM', ' BAC', ' BRK-B', ' AXP', ' COF', ' C', ' WFC',
+' NVDA', ' AVGO', ' QCOM', ' TXN', ' MU', ' AMD', ' XLNX', ' MCHP', ' ADI', ' JPM', ' BAC', ' BRK-B', ' AXP', ' COF', ' C', ' WFC',
 ' USB', ' PNC', ' MS', ' GS', ' SCHW', ' RF', ' ICE', ' SPGI', ' CME', ' MSCI', ' BLK', ' BK', ' STT', ' AON', ' MET', ' GL', ' L',
 ' BSX', ' BIO', ' EW', ' MDT', ' SYK', ' ABT', ' TMO', ' DHR', ' A', ' IQV', ' MTD', ' PFE', ' JNJ', ' MRK', ' ABBV', ' AMGN', ' GILD',
 ' FISV', ' BMY', ' LYV', ' EA', ' ATVI', ' UNH', ' G' ,' HUM', ' LOW', ' HD', ' MCD', ' SBUX', ' NKE', ' BKNG', ' TJX', ' ROST', ' SPECIA', ' BLL',
@@ -84,7 +83,25 @@ stocks = (' AAPL', ' GOOG', ' AMZN', ' TSLA', ' FB', ' GME', ' MSFT', ' ADBE', '
 ' LLOY', ' RR', ' IAG', ' BARC', ' GLEN', ' HSBA', ' TSCO', ' NWG', ' SMDS', ' RDSB', ' STAN', ' TW', ' AV', ' BATS', ' NOK', ' QS', ' TDOC',
 ' PZZA', ' BB', ' LI', ' QQQ', ' SCR', ' ARKK', ' WAMCX', ' IEC', ' TCEHY', ' HLT', ' PINS', ' BIOPX', ' TWLO', ' UPLD', ' W', ' TNC', ' MPGFX',
 ' MAT', ' FCX', ' IAC', ' ETSY', ' PTON', ' EXPR', ' JAGX', ' CCIV', ' SNDL', ' ZOM', ' MOMO', ' MYO', ' MENXF', ' TM', ' SFTBF', ' NTDMF', ' FRCOF',
-' NPPXF', ' NNDNF', ' NTDOF', ' KDDIF', ' SHECF', ' MUFG', ' MRAAF', ' TAK', ' HMC', ' ITOCF', ' SVNDF', ' MFG', ' MIELF', ' ALPMF', ' 7832.T')
+' NPPXF', ' NNDNF', ' NTDOF', ' KDDIF', ' SHECF', ' MUFG', ' MRAAF', ' TAK', ' HMC', ' ITOCF', ' SVNDF', ' MFG', ' MIELF', ' ALPMF', ' 7832.T',
+' CGC', ' SMG', ' GWPH', ' TLRY', ' CRON', ' ACB', ' NBEV', ' CRBP', ' TGODF', ' TRTC', ' CANN', ' MJ', ' MSBHF', ' MITSY', ' SSUMY',
+' ITOCY', ' MARUY', ' TGC', ' TCDA', ' REPH', ' TPIC', ' GTT', ' ANGN', ' RAAS', ' HTOO', ' AGIO', ' SQQQ', ' SVC', ' CDNA', ' COKE',
+' AVXL', ' XTNT', ' AFI', ' FSR', ' QUIK', ' CD', ' RILY', ' SLCA', ' LMAT', ' HOVNP', ' QEMM', ' CNNE', ' AIG', ' NURE', ' RFDA',
+' PMAR', ' UHAL', ' FLQL', ' ABST', ' ACMR', ' ADTN', ' AEHR', ' AEY', ' AEYE', ' AFRM', ' AGMH', ' AGYS', ' AI', ' AKAM', ' AIRG',
+' ALGM', ' ALLT', ' ALOT', ' ALTR', ' ALYA', ' AMBA', ' AMST', ' AMSWA', ' ANET', ' ANSS', ' ANY', ' AOSL', ' APH', ' API', ' APPN',
+' ARRY', ' APPS', ' ARW', ' ASUR', ' ASYS', ' ATC', ' ATEN', ' ATOM', ' AUDC', ' AUUD', ' AVGO', ' AVNW', ' AVT', ' AXTI', ' AYX',
+' AZPN', ' BDR', ' BCOV', ' BELFB', ' BHE', ' BILL', ' BKI', ' BKTI', ' ABR', ' AAXJ', ' AAME', ' RDS.A', ' RDS.B', ' NTDOY', ' AVID',
+' TTWO', ' CCOEF', ' PLTK', ' ZNGA', ' SKLZ', ' UBSFY', ' OTGLY', ' GLUU', ' TSCRD', ' GRVY', ' NGMS', ' ENGMF', ' MSGM', ' SCPL', ' BRGGF',
+' INSE', ' RNWK', ' MLLLF', ' VS', ' GMGI', ' SLGG', ' BHAT', ' GIGM', ' YVR', ' F', ' PLUG', ' RKT', ' TSNP', ' RLLCF', ' BAYP',
+' LPSN', ' TRIP', ' WW', ' BLUE', ' SFIX', ' MMYT', ' VICR', ' DNLI', ' VST', ' ENV', ' FLR', ' SPCE', ' BKRKF', ' ZIJMF', ' FL',
+' REGI', ' APPH', ' FOCS', ' ROOT', ' ADT', ' EIPAF', ' NKLA', ' ABML', ' TSNPD', ' DJI', ' IXIC',
+' 9684.T', ' 9697.T', ' 9766.T', ' 7974.T', ' 3659.T', ' 7974.T', ' TOR', ' OTC', ' CX', ' BECN', ' JELD', ' FRTA', ' MI', ' EME',
+' BEKE', ' PLD', ' WELL', ' SPG-PJ', ' PSA-PK', ' PSA-PH', ' DLR', ' CBRE', ' WY', ' O', ' ARE', ' DLR-PJ', ' DLR-PC', ' INVH',
+' ESS', ' SCCO', ' NEM', ' MLM', ' MANU', ' ASR', ' MRAAF', ' JUVE.MI', ' RFC', ' SSL.MI', ' BVB.DE', ' AJAX.AS', ' ASR.MI',
+' RACE.MI', ' LINK-USD', ' XLM-USD', ' DOGE-USD', ' XEM-USD', ' XMR-USD', ' ATOM1-USD', ' DOT1-USD', ' DOT2-USD', ' BTC-USD', ' ETH-USD', ' BCH-USD',
+' ATOM2-USD', ' MIOTA-USD', ' THETA-USD', ' TRX-USD', ' BSV-USD', ' EOS-USD', ' SOL1-USD', ' SOL2-USD', ' USDC-USD', ' NEO-USD', ' XTZ-USD', ' VET-USD',
+' DASH-USD', ' ALGO-USD', ' CCOEY', ' KNMCY', ' SQNXF', ' SGAMY', ' ADA-USD', ' BNB-USD', ' XRP-USD', ' LTC-USD',
+' NCBDF')
 
 
 selected_stock = st.selectbox('Select stock you want to predict', stocks)
