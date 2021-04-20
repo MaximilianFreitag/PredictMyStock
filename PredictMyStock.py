bf17865d30f4190bf3d7569c0422d3103572ae92
Maximilian Freitag <46624616+MaximilianFreitag@users.noreply.github.com>
2021-04-20 11:26:41 +0200

    Update PredictMyStock.py

--- PredictMyStock.py
+++ PredictMyStock.py
@@ -153,14 +153,19 @@ def plot_raw_data():
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Volume'], name="Volume"))
-    fig.layout.update(title_text='Time Series Data without forcast', xaxis_rangeslider_visible=True)
+    fig.layout.update(title_text='Time Series Data of stock (without forcast)', xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
 
 plot_raw_data()
 
+def plot_raw_data_two():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Volume'], name="Volume of stock"))
+    fig.layout.update(title_text='Time Series Data of the volume  (without forcast)', xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
 
-
+plot_raw_data_two()
+        
 
 # Predict forecast with Prophet.
 df_train = data[['Date','Close']]
